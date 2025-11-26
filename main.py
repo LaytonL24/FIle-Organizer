d3d3b4ab68c7c56b388d4c136a7a57a54b000570
Layton Lin <laytonlin@Laytons-Laptop.local>
2025-11-26 03:27:29 -0500

    Added and updated code. Add minor comments.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import shutil
 
 source_dir = "/Users/laytonlin/Downloads"
 
-# To 
-extensions_map = {
+# To move - Extensions
+extensions_move_map = {
     ".jpg": "Pictures", 
     ".png": "Pictures", 
     ".docx": "Docs", 
@@ -14,21 +14,34 @@ extensions_map = {
     ".mov": "Movies",
 }
 
+# To Delete - Extensions
+extensions_delete_map = {
+    ".zip",
+    ".dmg"
+}
+
 for file in os.listdir(source_dir):
     file_path = os.path.join(source_dir, file)
     
     if os.path.isfile(file_path):
         extension = os.path.splitext(file)[1].lower()
         
-        if extension in extensions_map:
-            destination_name = extensions_map[extension]
+        if extension in extensions_move_map:
+            destination_name = extensions_move_map[extension]
             destination_folder = os.path.join(source_dir, destination_name)
+
+            # Creates folder if it doesn't exist
             os.makedirs(destination_folder, exist_ok=True)  
 
             destination_path = os.path.join(destination_folder, file)
             shutil.move(file_path, destination_path)
 
             print("Moved " + file + " to " + destination_path)
+    
+        elif extension in extensions_delete_map:
+            os.remove(file)
+
+            print("Deleted " + file)
         else:
             print("Unknown extension for " + file)
     else:
